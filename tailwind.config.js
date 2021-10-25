5a33d59b7eac6b5add1d50f67d09391f96d7f515
yamanidev <elamine.yamani@gmail.com>
2021-10-25 22:23:29 +0100

    Max-width breakpoint configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
     },
     darkMode: false, // or 'media' or 'class'
     theme: {
+        screens: {
+            "2xl": { "max": "1516px" },
+            xl: { "max": "1260px" },
+            lg: { "max": "1004px" },
+            md: { "max": "748px" },
+            sm: { "max": "630px" }
+        },
+        container: {
+
+            screens: {
+                sm: '620px',
+                md: '748px',
+                lg: '1004px',
+                xl: '1260px',
+                '2xl': '1516px'
+            }
+        },
         extend: {
             colors: {
                 "gradio-red": "#C0022F"
